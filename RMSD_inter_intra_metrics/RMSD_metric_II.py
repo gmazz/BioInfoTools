8d092317ed4da94ebc26c1cd363acccc84091fd1
gmazz <mazzocco.giovanni@gmail.com>
2014-09-10 17:01:06 +0200

    RMSD_metric_II.py progress

--- RMSD_inter_intra_metrics/RMSD_metric_II.py
+++ RMSD_inter_intra_metrics/RMSD_metric_II.py
@@ -8,34 +8,36 @@ import numpy
 class scoreObj(object):
 
     def __init__(self, ID_tuple):
-
         self.ID_tuple = ID_tuple
         self.RMSD_list = []
         self.TMscore_list = []
 
-
     def scores_update(self, RMSD, TMscore):
-
         self.RMSD = RMSD
         self.TMscore = TMscore
         self.RMSD_list.append(self.RMSD)
         self.TMscore_list.append(self.TMscore)
 
+    def give_RMSD(self):
+        return self.RMSD_list
 
-    def print_RMSD(self):
-        print self.RMSD_list
-        #for k, v in self.RMSD_list.iteritems():
-        #    print k, v
+    def give_TMscore(self):
+        return self.TMscore_list
 
 
-    def print_TMscore(self):
-        print self.TMscore_list
-       # for k, v in self.TMscore_list.iteritems():
-        #    print k, v
+class seroList(object):
 
+    def __init__(self, seroID, serolist):
+        self.seroID = seroID
+        self.serolist = serolist
+
+    def ID_get(self):
+        return self.seroID
+
+    def mlist_get(self):
+        return self.serolist
 
 def data_import(list_dir, scores_file):
-
     rootdir = os.getcwd()
     models_list_dir = '%s/%s/' %(rootdir, list_dir)
     list_files = os.listdir(models_list_dir)
@@ -43,48 +45,70 @@ def data_import(list_dir, scores_file):
     scores_file = '%s/%s' %(rootdir, scores_file)
     file_handle = open(scores_file)
     data_lines = file_handle.readlines()
-
     return list_files, data_lines
 
+def sero_obj_list_generation(model_dir, list_files):
+    sero_obj_list = []
+    for f in list_files:
+        fname = '%s/%s' %(model_dir, f)
+        seroID = f.split('.txt')[0]
+        file_handle = open(fname)
+        serolines = file_handle.readlines()
+        serolist = [m.split('\n', 1)[0] for m in serolines]
+        tmp_sero_obj = seroList(seroID, serolist)
+        sero_obj_list.append(tmp_sero_obj)
+    return sero_obj_list
 
 def list_combination(list_files):
-
     list_id = [l.split('.txt', 1)[0] for l in list_files]
-    list_combo = itertools.combinations(list_id, 2)
+    list_combo = itertools.combinations_with_replacement(list_id, 2)
+    #list_combo = [set(elem) for elem in list_combo]
     return list_combo
 
-def object_update(data_lines, objs):
+def model_sero_assoc(model, sero_obj_list):
+    sero_target = [obj.seroID for obj in sero_obj_list if model in obj.serolist]
+    return sero_target[0] #takes just one of the possible elements
+
+
+def object_update(data_lines, objs, sero_obj_list):
     for line in data_lines:
         line = line.split('\n')[0]
         values = line.split('\t')
-        cmb_target = (values[0].split('.pdb')[0], values[1].split('.pdb')[0])
-        print cmb_target
 
-        #
-        # Note the cmb_target is in the form ('AEO91855', 'AAW29080') while the obj.ID_tuple in ('H1N1v', 'H2Nx')
-        # A method is required to map the cmb_target in the supertypes.
-        #
-
+        model_a = values[0].split('.pdb')[0]
+        model_b = values[1].split('.pdb')[0]
+        sero_a = model_sero_assoc(model_a, sero_obj_list)
+        sero_b = model_sero_assoc(model_b, sero_obj_list)
+        serotuple = (sero_a, sero_b)
         RMSD = values[2]
         TMscore = values[3]
 
+        for obj in objs:
+            if (sorted(serotuple) == sorted(obj.ID_tuple)):
+                obj.scores_update(RMSD, TMscore)
 
-        #tar = [obj for obj in objs if obj.ID_tuple == cmb_target][0]
-        #tar.scores_update(RMSD, TMscore)
+def metrics(objs):
+    for o in objs:
+        print o.ID_tuple
+        print o.RMSD_list[1:10]
+        print o.TMscore_list[1:10]
+
+        # TO BE COMPLETED
 
 
 def main():
+    model_dir = 'model_lists'
+    list_files, data_lines = data_import(model_dir, 'all_vs_all_correct.txt')
+    sero_obj_list = sero_obj_list_generation(model_dir, list_files)
 
-    list_files, data_lines = data_import('model_lists', 'all_vs_all_correct.txt')
     list_combo = list_combination(list_files)
     objs = [scoreObj(cmb) for cmb in list_combo]
-    object_update(data_lines, objs)
-
+    object_update(data_lines, objs, sero_obj_list)
+    metrics(objs)
 
 
     #print data_lines
     #print ''.join([str(i) for i in list_combo]) # Tuples
-
 
 main()
 # for f in lst_files:
