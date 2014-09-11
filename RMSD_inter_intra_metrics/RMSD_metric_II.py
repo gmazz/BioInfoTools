9a5c62e376e2c0e6dfdd51447a05c1f4bfff2bc5
gmazz <mazzocco.giovanni@gmail.com>
2014-09-11 13:36:49 +0200

    RMSD_metric_II_final

--- RMSD_inter_intra_metrics/RMSD_metric_II.py
+++ RMSD_inter_intra_metrics/RMSD_metric_II.py
@@ -1,8 +1,6 @@
 import os, sys, re
 import itertools
-from collections import defaultdict
-import subprocess
-import numpy
+import numpy as np
 
 
 class scoreObj(object):
@@ -15,8 +13,18 @@ class scoreObj(object):
     def scores_update(self, RMSD, TMscore):
         self.RMSD = RMSD
         self.TMscore = TMscore
-        self.RMSD_list.append(self.RMSD)
-        self.TMscore_list.append(self.TMscore)
+        self.RMSD_list.append(float(self.RMSD))
+        self.TMscore_list.append(float(self.TMscore))
+
+    def RMSD_metric(self):
+        self.RMSD_np = np.array(self.RMSD_list)
+        self.RMSD_mms = [np.mean(self.RMSD_np), np.median(self.RMSD_np), np.std(self.RMSD_np), len(self.RMSD_np)]
+        return self.RMSD_mms
+
+    def TMscore_metric(self):
+        self.TMscore_np = np.array(self.TMscore_list)
+        self.TMscore_mms = [np.mean(self.TMscore_np), np.median(self.TMscore_np), np.std(self.TMscore_np), len(self.TMscore_np)]
+        return self.TMscore_mms
 
     def give_RMSD(self):
         return self.RMSD_list
@@ -37,6 +45,7 @@ class seroList(object):
     def mlist_get(self):
         return self.serolist
 
+
 def data_import(list_dir, scores_file):
     rootdir = os.getcwd()
     models_list_dir = '%s/%s/' %(rootdir, list_dir)
@@ -46,6 +55,7 @@ def data_import(list_dir, scores_file):
     file_handle = open(scores_file)
     data_lines = file_handle.readlines()
     return list_files, data_lines
+
 
 def sero_obj_list_generation(model_dir, list_files):
     sero_obj_list = []
@@ -59,15 +69,16 @@ def sero_obj_list_generation(model_dir, list_files):
         sero_obj_list.append(tmp_sero_obj)
     return sero_obj_list
 
+
 def list_combination(list_files):
     list_id = [l.split('.txt', 1)[0] for l in list_files]
     list_combo = itertools.combinations_with_replacement(list_id, 2)
-    #list_combo = [set(elem) for elem in list_combo]
     return list_combo
+
 
 def model_sero_assoc(model, sero_obj_list):
     sero_target = [obj.seroID for obj in sero_obj_list if model in obj.serolist]
-    return sero_target[0] #takes just one of the possible elements
+    return sero_target[0]
 
 
 def object_update(data_lines, objs, sero_obj_list):
@@ -80,88 +91,31 @@ def object_update(data_lines, objs, sero_obj_list):
         sero_a = model_sero_assoc(model_a, sero_obj_list)
         sero_b = model_sero_assoc(model_b, sero_obj_list)
         serotuple = (sero_a, sero_b)
-        RMSD = values[2]
-        TMscore = values[3]
+        RMSD = float(values[2])
+        TMscore = float(values[3])
 
         for obj in objs:
             if (sorted(serotuple) == sorted(obj.ID_tuple)):
                 obj.scores_update(RMSD, TMscore)
 
-def metrics(objs):
-    for o in objs:
-        print o.ID_tuple
-        print o.RMSD_list[1:10]
-        print o.TMscore_list[1:10]
 
-        # TO BE COMPLETED
+def metrics(objs):
+    for obj in objs:
+        ID_tuple = obj.ID_tuple
+        RMSD_metric = obj.RMSD_metric()
+        TMscore_metric = obj.TMscore_metric()
+        print "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s" %(ID_tuple[0], ID_tuple[1], format(RMSD_metric[0], '.3f'), format(RMSD_metric[1], '.3f'), format(RMSD_metric[2], '.3f'), format(TMscore_metric[0], '.3f'), format(TMscore_metric[1], '.3f'), format(TMscore_metric[2], '.3f'), TMscore_metric[3])
 
 
 def main():
     model_dir = 'model_lists'
-    list_files, data_lines = data_import(model_dir, 'all_vs_all_correct.txt')
+    input_score_file = 'test.txt'
+    list_files, data_lines = data_import(model_dir, input_score_file)
     sero_obj_list = sero_obj_list_generation(model_dir, list_files)
-
     list_combo = list_combination(list_files)
     objs = [scoreObj(cmb) for cmb in list_combo]
     object_update(data_lines, objs, sero_obj_list)
     metrics(objs)
 
 
-    #print data_lines
-    #print ''.join([str(i) for i in list_combo]) # Tuples
-
 main()
-# for f in lst_files:
-#     if '.pdb' in f:
-#         list_pdb.append(f)
-#
-#
-# def TM_RMSD():
-#
-#     RMSD_array = []
-#     TM_score_array = []
-#
-#     for f in pdb_pairs:
-#
-#         cmd = 'TMalign %s %s -a' %(f[0], f[1])
-#         result = subprocess.check_output(cmd, shell=True)
-#         ln_result = result.split('\n')
-#
-#
-#         RMSD_line = ln_result[16].split(',')[1]
-#         TM_score_line_1 = ln_result[17]
-#         TM_score_line_2 = ln_result[18]
-#
-#
-#         regexp_score = re.compile("([A-Za-z\s\W]+)([\d\.]+)")
-#
-#         RMSD = regexp_score.match(RMSD_line).group(2)
-#         TM_score_p1 = regexp_score.match(TM_score_line_1).group(2)
-#         TM_score_p2 = regexp_score.match(TM_score_line_2).group(2)
-#
-#         TM_max=max([float(TM_score_p1), float(TM_score_p2)])
-#
-#         RMSD_array.append(float(RMSD))
-#         TM_score_array.append(float(TM_max))
-#
-#         print "%s\t%s\t%s\t%s" % (f[0], f[1], RMSD, TM_max)
-#
-#     RMSD_np = numpy.array(RMSD_array)
-#     TM_score_np = numpy.array(TM_score_array)
-#
-#     RMSD_mean = numpy.mean(RMSD_np)
-#     RMSD_median = numpy.median(RMSD_np)
-#     RMSD_std = numpy.std(RMSD_np)
-#
-#     TM_mean = numpy.mean(TM_score_np)
-#     TM_median = numpy.median(TM_score_np)
-#     TM_std = numpy.std(TM_score_np)
-#
-#
-#     print "\nRMSD_mean: %s\tRMSD_median: %s\tRMSD_std: %s\nTM_score_mean: %s\tTM_score_median: %s\tTM_score_std: %s" % (RMSD_mean, RMSD_median, RMSD_std, TM_mean, TM_median, TM_std)
-#
-# pdb_pairs = itertools.combinations(list_pdb, 2)
-# pairs_number = len(list(itertools.combinations(list_pdb, 2)))
-# TM_RMSD()
-#
-# print "Total pairs number: %s" %pairs_number
