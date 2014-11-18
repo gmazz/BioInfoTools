bb49227027d45c1fb13442ee4364a1041b36cd19
gmazz <mazzocco.giovanni@gmail.com>
2014-11-18 17:25:20 +0100

    add model_gen dir

---
+++ model_gen/run_auto.py
@@ -0,0 +1,28 @@
+import re, sys, os, shutil
+
+rootdir = os.getcwd()
+
+def generate_path():
+
+	newpath = '%s/DONE' %rootdir 
+	if not os.path.exists(newpath): os.makedirs(newpath)
+
+def automate():
+	
+	
+	list = open("list.txt", "r")
+	line = list.readline()
+	line = line.rstrip('\n') 
+	seq_list = line.split(',')
+	for seq in seq_list:
+
+		newPath = '%s/%s/' %(rootdir,seq)
+		os.chdir(newPath)
+		print os.getcwd()
+		os.system("python model.py > model.log")
+		dst = '%s/DONE' %rootdir
+		shutil.move(newPath, dst)	
+
+
+generate_path()
+automate()
