b857fcedcf8733e259a5f7af8938f4b8f3f1e148
gmazz <mazzocco.giovanni@gmail.com>
2015-03-10 17:51:06 +0100

    …

--- get_fasta/get_fasta.py
+++ get_fasta/get_fasta.py
@@ -4,25 +4,35 @@ import urllib.request
 import contextlib
 
 
-def get_id_list(id_file_list):
-    handle = open(id_file_list, 'r+')
+def get_id_list(id_file_name):
+    handle = open(id_file_name, 'r+')
     ids = handle.readlines()
     id_list = [id.rstrip('\n').split('\t')[0] for id in ids]
     return id_list
 
-def fasta_fetch(id_list):
+def fasta_fetch(id_list, id_file_name):
+    multifasta_name = id_file_name.replace("_list.txt", "_multi.fas")
+    multifasta_out = open(multifasta_name, 'w+')
+
     for id in id_list:
         with contextlib.closing(urllib.request.urlopen("http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=protein&id=" + id + "&rettype=fasta&retmode=text")) as url:
             fasta = url.read().decode('utf8')
             fasta = io.StringIO(fasta)
             records = list(SeqIO.parse(fasta, 'fasta'))
-            print (records)
+            if len(records) == 1:
+                print (records[0].description)
+                print (records[0].seq)
+                multifasta_out.write(str('>' + records[0].description + '\n'))
+                multifasta_out.write(str(records[0].seq + '\n'))
+
+            else:
+                sys.exit("%s has multiple entries, for chain A. Please check it out!" % id)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print ("\n Please indicate the ID_list file. e.g.: $ get_fasta.py ID_list.txt\n")
 
-    id_file_list = sys.argv[1]
-    id_list = get_id_list(id_file_list)
-    fasta_fetch(id_list[0:1])
+    id_file_name = sys.argv[1]
+    id_list = get_id_list(id_file_name)
+    fasta_fetch(id_list, id_file_name)
 
