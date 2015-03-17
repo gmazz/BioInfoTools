17d10d260de2759b563f193a785fce67b587b146
gmazz <mazzocco.giovanni@gmail.com>
2015-03-17 22:00:56 +0100

    modifications on reading results structure

--- PDB_template_fetching/pdb_template_fetch_2.py
+++ PDB_template_fetching/pdb_template_fetch_2.py
@@ -13,6 +13,7 @@ def data_import(data_file):
     records = list(SeqIO.parse(data_file, 'fasta'))
     return records
 
+
 def type_set(bl):
     bl[0] = str(bl[0].split('|')[3])
     bl[1] = str(bl[1].split('|')[0])
@@ -34,7 +35,7 @@ def search_hit(record, cutoff):
     blast_cmd = '/usr/local/ncbi/blast/bin/blastp'
     blast_db = '%s/HA_db/HA_pdb' % cwd
     SeqIO.write(record, "blast_query.fas", "fasta")
-    blast_output = NcbiblastpCommandline(cmd=blast_cmd, db=blast_db, evalue=cutoff, outfmt=6, query="blast_query.fas")()[0]#Blast command
+    blast_output = NcbiblastpCommandline(cmd=blast_cmd, db=blast_db, evalue=cutoff, outfmt=6, query="blast_query.fas")()[0] #Blast command
     blast_list = blast_output.rstrip('\s').split('\n')[:-1]
 
     blast_header = [
@@ -59,32 +60,36 @@ def search_hit(record, cutoff):
         type_set(bl)
         bl_dict = dict(zip(blast_header, bl))
         record_dict[record.id].append(bl_dict)
-
     ordered_dict_list = sorted(record_dict[record.id], key=itemgetter('identity'), reverse=True)
     record_dict[record.id] = ordered_dict_list
     return record_dict
 
+
 def read_results(results, bhn):
     for tmp in results:
         for k, v in tmp.items():
-
-            subjects_id = [i['subject_id'] for i in v[0:bhn]]
-            evalues = [i['evalue'] for i in v[0:bhn]]
-            message = list(zip(subjects_id, evalues))
+            subject_id = [i['subject_id'] for i in v[0:bhn]]
+            evalue = [i['evalue'] for i in v[0:bhn]]
+            alignment_length = [i['alignment_length'] for i in v[0:bhn]]
+            message = list(zip(subject_id, evalue, alignment_length))
             print (k, message)
+
 
 def iterate(data):
     results = [search_hit(record, data['cutoff']) for record in data['records']]
     return results
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print ("\n Please indicate the data.fas file. e.g.: $ pdb_template_fetch.py test.fas\n")
 
-data = {}
-data['data_file'] = sys.argv[1]
-data['records'] = data_import(data['data_file'])
-data['cutoff'] = 75
-best_hits_number = 1
-results = iterate(data)
-read_results(results, best_hits_number)
+def main():
+    if __name__ == '__main__':
+        if len(sys.argv) != 2:
+            print ("\n Please indicate the data.fas file. e.g.: $ pdb_template_fetch.py test.fas\n")
+    data = {}
+    data['data_file'] = sys.argv[1]
+    data['records'] = data_import(data['data_file'])
+    data['cutoff'] = 75
+    best_hits_number = 10
+    results = iterate(data)
+    read_results(results, best_hits_number)
+
+main()
