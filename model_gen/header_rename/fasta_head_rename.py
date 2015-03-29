dc1f51ef33f4f623cc1fd8c4f5a32471e0a86ceb
gmazz <mazzocco.giovanni@gmail.com>
2015-03-29 16:56:59 +0200

    ….

--- model_gen/header_rename/fasta_head_rename.py
+++ model_gen/header_rename/fasta_head_rename.py
@@ -3,27 +3,31 @@ from Bio import Entrez
 import os, re, sys, io
 from os import listdir
 from os.path import isfile, join
+import contextlib
 
-def rename(file):
-    records = list(SeqIO.parse(file, 'fasta'))
-    file_handle = open(id_list, 'r')
-    file_handle = file_handle.readlines()
-    template_name = "fasta_results/%s_target.fas" %(file_handle[0].rstrip('\n').split('\t')[1])
-    file_out = open(template_name, 'w+')
 
-    id_list = [id.split('\t')[0] for id in file_handle]
-    for id in id_list:
-        for r in records:
-            if id in r.id:
-                entry = ">%s\n%s\n" %(r.id, r.seq)
-                file_out.write(entry)
-    file_out.close()
+def rename(file, file_dir):
+    new_file_name = "fasta_renamed/%s,my_alignment.fst" %(file.split('.')[0])
+    file_out = open(new_file_name, 'w+')
+
+    file_path = "%s/%s" %(file_dir, file)
+    records = list(SeqIO.parse(file_path, 'fasta'))
+    template = records[0]
+    new_ref_id = records[0].id.replace("_ref", "")
+    new_ref = ">%s\n%s\n" %(new_ref_id, records[0].seq)
+    file_out.write(new_ref)
+
+    for r in records[1:]:
+        new_target_id = r.id.split('|')[3]
+        new_target = ">%s\n%s\n" %(new_target_id, r.seq)
+        file_out.write(new_target)
+
 
 def main():
     cwd = os.getcwd()
-    list_dir = "%s/fasta_targets" % cwd
-    file_list = [f for f in listdir(list_dir) if isfile(join(list_dir, f))]
+    file_dir = "%s/fasta_targets" % cwd
+    file_list = [f for f in listdir(file_dir) if isfile(join(file_dir, f))]
     for file in file_list:
-        rename(file)
+        rename(file, file_dir)
 
 main()
