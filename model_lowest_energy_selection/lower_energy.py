02eb9b9e15b02b9474b4bb5417fbce1f7b412190
gmazz <mazzocco.giovanni@gmail.com>
2015-03-30 21:50:41 +0200

    …

--- model_lowest_energy_selection/lower_energy.py
+++ model_lowest_energy_selection/lower_energy.py
@@ -7,24 +7,30 @@ from os.path import isfile, join
 
 
 def read(file):
-	temp_dict = {}
-	fileh = open(file, 'r+')
-	lines = fileh.readlines()
-	for l in lines:
-		val = l.split('\n')[0].split(' ')
-		if len(val) == 2:
-		    temp_dict.update({val[0]: float(val[1])})
-	
-	v = sorted(temp_dict.items(), key=lambda x:x[1])
-	return v[0]	
+    temp_dict = {}
+    fileh = open(file, 'r+')
+    lines = fileh.readlines()
+    for l in lines:
+        val = l.split('\n')[0].split(' ')
+        if len(val) == 2:
+            temp_dict.update({val[0]: float(val[1])})
+
+    v = sorted(temp_dict.items(), key=lambda x: x[1])
+    return v[0]
+
 
 def main():
-    	rootdir = os.getcwd()
-	files = [f for f in listdir(rootdir) if isfile(join(rootdir,f))]
-  	for file in files:
-		if file.endswith('.txt'):
-			best = read(file)
-			print best[0]
+    rootdir = os.getcwd()
+    files = [f for f in listdir(rootdir) if isfile(join(rootdir, f))]
+    for file in files:
+        if file.endswith('.txt'):
+            best = read(file)
+            print best[0]
 
 
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print "\n Please use template.ID as an argument. e.g.: $lower_energy.py energy_file.txt\n"
+    fl = read()
+    template = sys.argv[1]
 main()
