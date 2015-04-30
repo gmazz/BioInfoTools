02b427d5acf18840edb0f5c8a3863ee7f6274dbb
gmazz <mazzocco.giovanni@gmail.com>
2015-04-30 22:37:36 +0200

    __

--- fasta_selection&metadata/fasta_metadata_extraction.py
+++ fasta_selection&metadata/fasta_metadata_extraction.py
@@ -30,7 +30,8 @@ def extract_data(rec):
         return raw_data, id
 
 def data_write(fasta_file):
-    rec_dir = {'rec' : {}}
+    #rec_dir = {'rec' : {}}
+    rec_dir = {}
     #file_out = open('HA_new_models_classes.csv', 'w')
     my_set = set()
     records = list(SeqIO.parse(fasta_file, 'fasta'))
@@ -41,12 +42,11 @@ def data_write(fasta_file):
         if len(raw_data) == 5:
             rec_dir[id] = {'chain': raw_data[0], 'host': raw_data[1], 'location': raw_data[2], 'year': raw_data[4], 'line_length': 5}
         elif len(raw_data) == 4:
-            rec_dir[id] = {'chain': raw_data[0], 'location': raw_data[1], 'year': raw_data[3], 'line_length': 4}
+            rec_dir[id] = {'chain': raw_data[0], 'host': '-', 'location': raw_data[1], 'year': raw_data[3], 'line_length': 4}
         elif len(raw_data) == 3:
-            rec_dir[id] = {'chain': raw_data[0], 'location': raw_data[1], 'year': raw_data[2], 'line_length': 3}
+            rec_dir[id] = {'chain': raw_data[0], 'host': '-', 'location': raw_data[1], 'year': raw_data[2], 'line_length': 3}
         else:
             print "Missing case, please check %s" %rec
-
     #print my_set
     return rec_dir
 
@@ -54,30 +54,48 @@ def data_write(fasta_file):
 
 
 def data_filtering(rec_dir):
-
     host_renaming = {}
     host_renaming ['avian'] = ['murre', 'crane', 'grebe', 'bustard', 'coot', 'waterfowl', 'turtledove', 'chukkar', 'stint', 'anas', 'garganey', 'ostrich', 'pheasant', 'falcon', 'goose', 'chicken', 'turkey', 'duck', 'mallard', 'winged', 'sanderling', 'bird','quail','pelican', 'teal', 'shoveler', 'turnstone', 'knot', 'pintail', 'heron', 'gull', 'sandpiper']
     host_renaming ['swine'] = ['sw']
-
     for k, v in rec_dir.items():
         try:
             for k1, v1 in host_renaming.items():
+                # Substitute host names
                 m_streight = [k1 for s in v1 if v['host'].lower() in s.lower()]
                 m_reverse = [k1 for s in v1 if s.lower() in v['host'].lower()]
                 check = len(m_streight) + len(m_reverse)
                 if check > 0:
                     rec_dir[k]['host'] = k1
-            print k, v
         except:
             pass
-            #print k
+        try:
+            # Fetch correct year
+            year = clear_year(v['year'])
+            rec_dir[k]['year'] = year
+        except:
+            rec_dir[k]['year'] = '-'
+            print "ALARM!", k, v
+    return rec_dir
 
+def clear_year(raw_year):
+    year = raw_year.split('(')[0]
+    year = year.rstrip(' ')
+    if len(year) == 2:
+        if int(year) <= 15:
+            year = int(year) + 2000
+        else:
+            year = int(year) + 1900
+    try:
+        return int(year)
+    except:
+        return int(year)
 
 cwd = os.getcwd()
 #fasta_file = 'HA_sequences_2nd_modeling.fas'
 fasta_file = 'unique.fas'
 rec_dir = data_write(fasta_file)
-#print rec_dir, len(rec_dir)
+rec_dir = data_filtering(rec_dir)
 
-data_filtering(rec_dir)
-
+for k, v in rec_dir.items():
+    if v:
+        print "%s,%s,%s,%s,%s" %(k, v['chain'], v['host'], v['location'], v['year'])
