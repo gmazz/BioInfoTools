f772f4eac6c9e1f8f24dc7550b68a48f22732307
gmazz <mazzocco.giovanni@gmail.com>
2015-05-04 18:26:10 +0200

    blast_all_vs_all

---
+++ all_vs_all_blast/all_vs_all_blast.sh
@@ -0,0 +1,4 @@
+#multi_fasta=$($echo pwd)/test_HA_sequences_db2.fas;
+multi_fasta=$($echo pwd)/HA_sequences_db2.fas
+makeblastdb -in $multi_fasta -dbtype prot -out my_prot_blast_db;
+blastp -db my_prot_blast_db -query $multi_fasta -outfmt 6 -out all-vs-all_blast.tsv -num_threads 24
