78455ff32a7e1c28399e0367f874a7de9f937631
gmazz <mazzocco.giovanni@gmail.com>
2015-05-06 19:59:15 +0200

    crystal on all-vs-all_blast

--- all_vs_all_blast/all_vs_all_blast.sh
+++ all_vs_all_blast/all_vs_all_blast.sh
@@ -1,4 +1,4 @@
-#multi_fasta=$($echo pwd)/test_HA_sequences_db2.fas;
-multi_fasta=$($echo pwd)/HA_sequences_db2.fas
+multi_fasta=$($echo pwd)/crystals_local.fas
+#multi_fasta=$($echo pwd)/HA_sequences_db2.fas
 makeblastdb -in $multi_fasta -dbtype prot -out my_prot_blast_db;
-blastp -db my_prot_blast_db -query $multi_fasta -outfmt 6 -out all-vs-all_blast_nil.tsv -num_threads 24
+blastp -db my_prot_blast_db -query $multi_fasta -outfmt 6 -out crystals_blastp.tsv -num_threads 8
