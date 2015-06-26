7fe3a03b8e01b85c12feb6ea009a3f735799055a
gmazz <mazzocco.giovanni@gmail.com>
2015-06-26 11:37:36 +0200

    __

--- ProDY/GNM.py
+++ ProDY/GNM.py
@@ -1,6 +1,7 @@
 from prody import *
 import numpy as np
 from matplotlib.pylab import *
+import operator
 
 
 def protein_analysis(protein):
@@ -12,15 +13,25 @@ def protein_analysis(protein):
     print(dir(gnm))
     mat_cov = gnm.getCovariance()
     diag_mat_cov = np.diag(mat_cov)
-    print (diag_mat_cov)
-    #print (gnm.getVariances)
-    #kirch_mat = gnm.getKirchhoff()
-    #kirch_mat_inv = np.linalg.inv(kirch_mat)
-    #kirch_diag_inv = np.diag(kirch_mat_inv)
+    c = 1
+    dict_mobility = {}
+    for i in diag_mat_cov:
+        dict_mobility[c] = i
+        #print ("%s\t%s\n" %(c, i))
+        c = c+1
+    dict_mobility_sorted = dict_sorting(dict_mobility)
+    return dict_mobility
+
+
+def dict_sorting(dict):
+    sorted_by_key = sorted(dict.items(), key=operator.itemgetter(0))
+    sorted_by_val = sorted(dict.items(), key=operator.itemgetter(1))
+    return sorted_by_val
 
 def main(prot_list):
     for protein in prot_list:
-        protein_analysis(protein)
+        dict_mobility = protein_analysis(protein)
+        print dict_mobility.values()
 
 #prot_list = ['1aar', '1p38']
 prot_list = ['./4bgx.pdb']
