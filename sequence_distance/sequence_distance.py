e98b36fa81c2fe6cc8ecfea846dbd92ade4e1336
gmazz <mazzocco.giovanni@gmail.com>
2015-08-04 01:06:39 +0200

    pairwise2

--- sequence_distance/sequence_distance.py
+++ sequence_distance/sequence_distance.py
@@ -2,7 +2,7 @@ import os, sys, re
 import itertools
 from Bio import SeqIO
 from Bio import pairwise2
-
+from Bio.SubsMat import MatrixInfo as matlist
 #import subprocess
 #from multiprocessing import Pool
 #import affinity
@@ -21,20 +21,33 @@ def generate_dict(fasta_file):
     return data_dict
 
 
-def iterate(data_dict):
+def iterate(data_dict, parameters, fasta_file):
+    out_file_name = "%s.csv" %(fasta_file.split('.fas')[0])
+    out_file = open (out_file_name, "w")
     id_pairs = itertools.combinations(data_dict.keys(), 2)
     #pairs_number = len(list(itertools.combinations(data_dict.keys(), 2)))
     for i in id_pairs:
-        seq_1 = data_dict[i[0]]
-        seq_2 = data_dict[i[1]]
+        try:
+            seq_1 = data_dict[i[0]]
+            seq_2 = data_dict[i[1]]
+            message = "%s,%s,%s\n" %(i[0],i[1], str(pairwise2.align.globalds(seq_1, seq_2, parameters['matrix'], parameters['gap_open'], parameters['gap_extended'])[0][2]))
+            out_file.write(message)
+        except:
+            print "\nI do have problems with the following sequence pair: %s\n" %(str(i))
+        #print i, pairwise2.align.globaldx(seq_1, seq_2, matrix)
 
 
 #TM_RMSD()
 #print "Total pairs number: %s" %pairs_number
 
 def main():
-    fasta_file = 'crystals.fas'
+    fasta_file = 'test.fas'
+    parameters = {
+                    'matrix': matlist.blosum62,
+                    'gap_open': -10,
+                    'gap_extended': -0.5
+                }
     data_dict = generate_dict(fasta_file)
-    iterate(data_dict)
+    iterate(data_dict, parameters, fasta_file)
 
 main()
