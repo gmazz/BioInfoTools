7a4b7febc0ad6a35570eb111f4d52a32f09b4a30
gmazz <mazzocco.giovanni@gmail.com>
2015-08-10 14:08:05 +0200

    …

--- sequence_omogenize/sequence_omogenize.py
+++ sequence_omogenize/sequence_omogenize.py
@@ -13,11 +13,50 @@ def generate_dict(fasta_file):
     return data_dict
 
 
-def select(fasta_c_dict, fasta_l_dict, parameters):
+def print_align(fasta_c_dict, fasta_l_dict, parameters):
     for k in fasta_c_dict.keys():
         print '>%s_pdb\n%s' %(k, str(pairwise2.align.globalds(fasta_c_dict[k], fasta_l_dict[k], parameters['matrix'], parameters['gap_open'], parameters['gap_extended'])[0][0]))
         print '>%s_seq\n%s' %(k, str(pairwise2.align.globalds(fasta_c_dict[k], fasta_l_dict[k], parameters['matrix'], parameters['gap_open'], parameters['gap_extended'])[0][1]))
 
+
+def iterate_selection(fasta_c_dict, fasta_l_dict, parameters):
+    #for k in fasta_c_dict.keys():
+    k = ""
+    seq = list(pairwise2.align.globalds(fasta_c_dict[k], fasta_l_dict[k], parameters['matrix'], parameters['gap_open'], parameters['gap_extended'])[0][0])
+    pdb = list(pairwise2.align.globalds(fasta_c_dict[k], fasta_l_dict[k], parameters['matrix'], parameters['gap_open'], parameters['gap_extended'])[0][1])
+    select (seq, pdb)
+
+#def select(seq, pdb):
+#    while True:itertools
+#        if dash(number):
+#            yield number
+#        number += 1
+
+"""
+Initiating using generators
+"""
+
+def readfiles(filenames):
+    for f in filenames:
+        for line in open(f):
+            yield line
+
+def grep(pattern, lines):
+    return (line for line in lines if pattern in lines)
+
+def printlines(lines):
+    for line in lines:
+        print line,
+
+def main(pattern, filenames):
+    lines = readfiles(filenames)
+    lines = grep(pattern, lines)
+    printlines(lines)
+
+
+"""
+Finishing the use of generators
+"""
 
 def main():
     parameters = {
@@ -30,6 +69,6 @@ def main():
     fasta_l = 'long_sequences_chA.fas'
     fasta_c_dict = generate_dict(fasta_c)
     fasta_l_dict = generate_dict(fasta_l)
-    select(fasta_c_dict, fasta_l_dict, parameters)
+    iterate_selection(fasta_c_dict, fasta_l_dict, parameters)
 
 main()
