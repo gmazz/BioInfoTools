34a76fab0c4493fbff6bc4146223e282b36397cd
gmazz <mazzocco.giovanni@gmail.com>
2015-08-12 15:39:47 +0200

    HA_serotype_grouping.py (Simple but good mix of pandas and iterators)

--- HA_serotypes_grouping/sero_grouping.py
+++ HA_serotypes_grouping/sero_grouping.py
@@ -1,21 +1,57 @@
-import numpy as np
+#import numpy as np
 import pandas as pd
-import time
 
 
+"""
+The NesteDict iterator class generates a nested dictionary to be easily filled and accessed.
+Usage:
+
+    >> To generate nested dicts and store values
+    >> Note '...' is used in the example to represent an arbitrary number of level, is not a command symbol:
+
+    nd = NestedDict()
+    nd['level_1']['level_2'] ... ['level_n'] = 'value'
+
+    >> To store a specific value:
+
+    value =  nd['level_1']['level_2'] ... ['level_n']
+"""
 
 
-def import_data():
+class NestedDict(dict):
+    def __getitem__(self, item):
+        try:
+            return dict.__getitem__(self, item)
+        except KeyError:
+            value = self[item] = type(self)()
+            return value
+
+
+def import_data(par):
+
+    #Istantiate
+    pairs_distance = NestedDict()
 
     #Generate serogroups
-    sero_c = pd.read_csv('./sero_lists/crystals_sero_list.csv', index_col='p_id')
-    gb_c = sero_c.groupby('sero').groups
+    cl = pd.read_csv(par['data'], index_col=par['index_c'])
+    cl_groups = cl.groupby('sero').groups
 
-    #Generate dict containing protein_pair:distance
-    #df_c = pd.read_csv('./distances/crystals_rmsd.csv', index_col='distance')
-    #dfg_c = df_c.groupby(['p_id', 'p_id2']).groups
-    #print dfg_c
+    #Generate  containing protein_pair:distance
+    df = pd.read_csv(par['distance_data'], index_col=None)
+    for index, row in df.iterrows():
+        pairs_distance[row['p_id']][row['p_id2']] = row['distance']
+
+    return cl_groups, pairs_distance
 
 
+def main():
+    crystals_parameters = {
+        'data' : './sero_lists/crystals_sero_list.csv',
+        'index_c' : 'p_id',
+        'distance_data' : './distances/crystals_rmsd.csv'
+        }
 
-import_data()
+    sero, pairs_distance = import_data(crystals_parameters)
+
+
+main()
