e736ab58478cb7614824f525e849e44397649fb9
gmazz <mazzocco.giovanni@gmail.com>
2015-08-13 02:43:17 +0200

    —

--- HA_serotypes_grouping/sero_grouping.py
+++ HA_serotypes_grouping/sero_grouping.py
@@ -45,43 +45,49 @@ def import_data(par):
     return cl_groups, pairs_distance
 
 """
-This function compute the average distance within each serotype group.
+This function compute the average distance WITHIN each serotype group.
 All the permutation of the elements of each 'sero' dict entry are initially computed.
 Permutations are used instead of combination because we don't know if we have:
 'a,b,distance' or '(b,a,distance)' for a given pair.
 Hence both options are considered. Anyway if one option is found, the inverted one is discarded
 (as the results are equal)
 """
-def merge(sero, pair_distance):
+
+def within(sero, pair_distance):
+
     done = []
     tmp_distance_list = []
     sero_main_values = dict()
-    #for k, p_list in sero.items():
-    #    print k, p_list
-    tmp_permutations = itertools.permutations(sero['H10N7'], 2)
 
-   # print '4cyw,4cz0,' + str(pair_distance['4cyw']['4cz0'])
-   # print '4cz0,4cyw,' + str(pair_distance['4cz0']['4cyw'])
-    for p in tmp_permutations:
-        print p
-        tmp_distance = pair_distance[p[0]][p[1]]
-        if tmp_distance:
-            tmp_distance_list.append(tmp_distance)
-            done.append(p)
-            done.append(p[::-1]) #Add the inverse tuple to
-    print tmp_distance_list
-    #sero_main_values = {'H10N7':[np.mean(tmp_distance_list), np.median(tmp_distance_list), np.std(tmp_distance_list)]}
-    #print sero_main_values
+    for k, p_list in sero.items():
+        if len(p_list) == 1:
+            print ">>The %s element is alone in his serotype, no distances values present within this serotype" %(p_list[0])
+        else:
+            tmp_permutations = itertools.permutations(p_list, 2)
+            for p in tmp_permutations:
+                tmp_distance = pair_distance[p[0]][p[1]]
+                if tmp_distance:
+                    tmp_distance_list.append(tmp_distance)
+                    done.append(p)
+                    done.append(p[::-1]) #Add the inverse tuple to
+            sero_main_values = {k: [np.mean(tmp_distance_list), np.median(tmp_distance_list), np.std(tmp_distance_list)]}
+            tmp_distance_list = []
+            print sero_main_values
+
+
+def between():
+    pass
+
 
 def main():
     crystals_parameters = {
-        'data' : './sero_lists/crystals_sero_list.csv',
+        'data' : './sero_lists/crystals_sero_only.csv',
         'index_c' : 'p_id',
         'distance_data' : './distances/crystals_rmsd.csv'
         }
 
     c_sero, c_pair_distance = import_data(crystals_parameters)
-    merge(c_sero, c_pair_distance)
+    within(c_sero, c_pair_distance)
 
 main()
 
