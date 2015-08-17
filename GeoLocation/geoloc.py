48a0195050d86601baf3492ab816f9569557ff25
gmazz <mazzocco.giovanni@gmail.com>
2015-08-17 19:41:42 +0200

    …

--- GeoLocation/geoloc.py
+++ GeoLocation/geoloc.py
@@ -1,14 +1,26 @@
+import re
+import pandas as pd
 from geopy.geocoders import Nominatim
 
 
-
-
+def iterate(filename):
+    df = pd.read_csv(filename, index_col='id')
+    df['State'] = df.apply(lambda row: find_state(row['location']), axis=1)
+    print df
 
 def find_state(location):
-    geolocator = Nominatim()
-    loc = geolocator.geocode(location, language='en')
-    state = loc.address.split(',')
-    print state
+    location = re.sub(r"(\w)([A-Z])", r"\1 \2", location) #(e.g. BrevigMission => Brevig Mission)
+    try:
+        geolocator = Nominatim()
+        loc = geolocator.geocode(location, language='en')
+        state = loc.address.split(',')[-1]
+        return state
+    except:
+        return "Not_found"
 
-location = "NY"
-find_state(location)
+
+#iterate(filename='./crystals_metadata.csv')
+iterate(filename='./test.csv')
+
+
+#find_state(raw_locations)
