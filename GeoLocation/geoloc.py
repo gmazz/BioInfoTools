458a3fa2ae4436026c6264ab8ae22d16bce5d601
gmazz <mazzocco.giovanni@gmail.com>
2015-08-19 15:02:27 +0200

    —

--- GeoLocation/geoloc.py
+++ GeoLocation/geoloc.py
@@ -5,16 +5,16 @@ from geopy.geocoders import Nominatim
 
 def iterate(filename):
     df = pd.read_csv(filename, index_col='id')
-    df['State'] = df.apply(lambda row: find_state(row['location']), axis=1)
-    print df
+    df['Country'] = df.apply(lambda row: find_state(row['location']), axis=1)
+    #print df
 
 def find_state(location):
     location = re.sub(r"(\w)([A-Z])", r"\1 \2", location) #(e.g. BrevigMission => Brevig Mission)
     try:
         geolocator = Nominatim()
         loc = geolocator.geocode(location, language='en')
-        state = loc.address.split(',')[-1]
-        return state
+        country = loc.address.split(',')[-1]
+        return country
     except:
         return "Not found"
 
