51af9e739c7071c4da84a25b9ac8663e35fb1606
gmazz <mazzocco.giovanni@gmail.com>
2015-08-19 23:30:23 +0200

    ..

--- GeoLocation/geoloc.py
+++ GeoLocation/geoloc.py
@@ -1,56 +1,43 @@
 #!/usr/bin/python
 import re
-import math
 import pandas as pd
 from geopy.geocoders import Nominatim
 from incf.countryutils import transformations
+import requests
+
 
 def iterate(filename):
     df = pd.read_csv(filename, index_col='id')
-    df['Continent'] = df.apply(lambda row: find_state(row['location']), axis=1)
-    #df.to_csv("crystals_metadata_II.csv", sep=',')
+    df['continent'] = df.apply(lambda row: find_state(row['location']), axis=1)
+    print df
+    #df.to_csv("models_metadata_II.csv", sep=',')
 
 def find_state(location):
     if pd.notnull(location):
-        location = re.sub(r"(\w)([A-Z])", r"\1 \2", location) #(e.g. BrevigMission => Brevig Mission)
-        #try:
-        geolocator = Nominatim()
-        loc = geolocator.geocode(location, language='en')
-        country = loc.address.split(',')[-1].strip() # Get the country
-        tmp_country = country.split(' ')
-        tmp_country.append(country)
-        tmp_country = set(tmp_country)
-        ########################################################
-
-        print tmp_country
-
-        a = [transformations.cn_to_ctn(t) for t in tmp_country]
-
-        for t in tmp_country:
-            try:
-            except:
-
-
-        print a
-
-            #print tmp_country
-            #tmp_continent = [transformations.cn_to_ctn(word) for word in tmp_country]
-
-            #print country, tmp_country, tmp_continent
-
-            #for word in country:
-            #    tmp_continent.append(transformations.cn_to_ctn(word)) # Get the continent
-            #tmp_continent = filter(None, tmp_continent)
-            #continent = '_'.join(tmp_continent)
-            #return continent
-
-        #except Exception as e:
-        #    print e
-        #    return "* Not found *"
+        try:
+            location = re.sub(r"(\w)([A-Z])", r"\1 \2", location) #(e.g. BrevigMission => Brevig Mission)
+           # geolocator = Nominatim()
+           # loc = geolocator.geocode(location, language='en')
+           # country = loc.address.split(',')[-1].strip() # Get the country
+            country_code = geolocalize(location)[0]['address_components'][-1]['short_name']
+            #country_code = geolocalize(country)[0]['address_components'][0]['short_name']
+            continent = transformations.cca_to_ctn(country_code)
+            #print country_code, continent
+            return continent
+        except:
+            print "Problem getting country"
+            return 'NaN'
     else:
-        print "No location available"
-        return ""
+        return 'NaN'
 
 
-iterate(filename='./crystals_metadata.csv')
-#iterate(filename='./test.csv')
+def geolocalize(address):
+   try:
+       r = requests.get("https://maps.googleapis.com/maps/api/geocode/json",
+                        params={"address": address})
+       return r.json()['results']
+   except:
+       return {}
+
+#iterate(filename='./models_metadata.csv')
+iterate(filename='./test.csv')
