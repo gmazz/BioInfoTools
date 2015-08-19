52caa1324be9cb374c2f3cdfa8ec7495d8fad363
gmazz <mazzocco.giovanni@gmail.com>
2015-08-19 16:30:49 +0200

    __

--- GeoLocation/geoloc.py
+++ GeoLocation/geoloc.py
@@ -1,26 +1,30 @@
+#!/usr/bin/python
 import re
+import math
 import pandas as pd
 from geopy.geocoders import Nominatim
-
+from incf.countryutils import transformations
 
 def iterate(filename):
     df = pd.read_csv(filename, index_col='id')
-    df['Country'] = df.apply(lambda row: find_state(row['location']), axis=1)
-    #print df
+    df['Continent'] = df.apply(lambda row: find_state(row['location']), axis=1)
+
 
 def find_state(location):
-    location = re.sub(r"(\w)([A-Z])", r"\1 \2", location) #(e.g. BrevigMission => Brevig Mission)
-    try:
-        geolocator = Nominatim()
-        loc = geolocator.geocode(location, language='en')
-        country = loc.address.split(',')[-1]
-        return country
-    except:
-        return "Not found"
+    if pd.notnull(location):
+        location = re.sub(r"(\w)([A-Z])", r"\1 \2", location) #(e.g. BrevigMission => Brevig Mission)
+        try:
+            geolocator = Nominatim()
+            loc = geolocator.geocode(location, language='en')
+            country = loc.address.split(',')[-1].strip() # Get the country
+            continent = transformations.cn_to_ctn(country) # Get the continent
+            return continent
+        except:
+            return "Not found"
+    else:
+        print "No location available"
+        return "NaN"
 
 
-#iterate(filename='./crystals_metadata.csv')
-iterate(filename='./test.csv')
-
-
-#find_state(raw_locations)
+iterate(filename='./crystals_metadata.csv')
+#iterate(filename='./test.csv')
