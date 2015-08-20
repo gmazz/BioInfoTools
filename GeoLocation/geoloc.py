80b53584bf4054ad95900c67d1fd7f1a7d098e80
gmazz <mazzocco.giovanni@gmail.com>
2015-08-20 18:54:18 +0200

    …

--- GeoLocation/geoloc.py
+++ GeoLocation/geoloc.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 import re
 import pandas as pd
-from geopy.geocoders import Nominatim
 from incf.countryutils import transformations
 import requests
 
@@ -9,23 +8,22 @@ import requests
 def iterate(filename):
     df = pd.read_csv(filename, index_col='id')
     df['continent'] = df.apply(lambda row: find_state(row['location']), axis=1)
-    print df
+    df
     #df.to_csv("models_metadata_II.csv", sep=',')
 
 def find_state(location):
     if pd.notnull(location):
-        try:
+        if len(location) > 2: #Avoid splitting country codes
             location = re.sub(r"(\w)([A-Z])", r"\1 \2", location) #(e.g. BrevigMission => Brevig Mission)
-           # geolocator = Nominatim()
-           # loc = geolocator.geocode(location, language='en')
-           # country = loc.address.split(',')[-1].strip() # Get the country
-            country_code = geolocalize(location)[0]['address_components'][-1]['short_name']
-            #country_code = geolocalize(country)[0]['address_components'][0]['short_name']
-            continent = transformations.cca_to_ctn(country_code)
-            #print country_code, continent
-            return continent
-        except:
-            print "Problem getting country"
+            request = ''
+            while not request:
+                try:
+                    request = geolocalize(location.strip())[0]['address_components'][-1]['short_name']
+                    continent = transformations.cca_to_ctn(request)
+                    print location, continent
+                    return continent
+                except:
+                    pass
             return 'NaN'
     else:
         return 'NaN'
